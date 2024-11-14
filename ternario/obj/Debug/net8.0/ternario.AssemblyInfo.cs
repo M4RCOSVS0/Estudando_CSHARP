a1206ee0319672c70c7735809b26674963f815df
M4RCOSVS0 <marcossvin.so@gmail.com>
2024-11-13 22:26:41 -0300

    add enum 3

--- ternario/obj/Debug/net8.0/ternario.AssemblyInfo.cs
+++ ternario/obj/Debug/net8.0/ternario.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ternario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf26cc73ddb9e6962e6aee5bacc660148454462")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+008105c04619f85bcb25a137c0a2900830b2b838")]
 [assembly: System.Reflection.AssemblyProductAttribute("ternario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ternario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
